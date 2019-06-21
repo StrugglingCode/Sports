81ff92a93ba755eee81e6ba5c6bc2b3d26275d44
Rishu Garg <rishugarg102@gmail.com>
2019-06-21 11:45:41 +0530

    SportAdapter
    created sportAdpter class to fill the gridview

--- app/src/main/java/com/example/sports/MainActivity.kt
+++ app/src/main/java/com/example/sports/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.sports
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var sportAdapter = SportAdapter(this@MainActivity )
+ grid_view.adapter = sportAdapter
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
---
+++ app/src/main/java/com/example/sports/SportAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.sports
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.sport_item.view.*
+import java.util.zip.Inflater
+
+class SportAdapter:BaseAdapter {
+
+    private  var context:Context? = null
+    private var sportDatabase:SportDatabase?=null
+
+    constructor(context: Context)
+    {
+        this.context = context
+        sportDatabase = SportDatabase()
+    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+var sport:Sport = sportDatabase?.sportList?.get(position) ?: Sport("No Name","No Details",R.id.image)
+         var inflater= context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE)as LayoutInflater
+         var sportItem = inflater.inflate(R.layout.sport_item,null)
+
+        sportItem.sport_image.setImageResource(sport._sportImage ?:R.drawable.placeholder)
+        sportItem.sport_textView.setText(sport._sportName)
+
+        return sportItem
+
+    }
+
+    override fun getItem(position: Int): Any {
+        return sportDatabase?.sportList?.get(position) ?: Sport("No Name","No Details",R.id.image)
+
+
+            }
+
+    override fun getItemId(index: Int): Long {                     //index of sport objects
+
+return index.toLong()
+    }
+
+    override fun getCount(): Int {
+      return sportDatabase?.sportList?.size ?: 0
+
+    }
+
+
+}
